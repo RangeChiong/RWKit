8c3297ee1aed05a194f35921c1ccd18f2ffca331
蒙牛D罩杯撸废 <rangeChiong@outlook.com>
2016-06-25 16:32:14 +0800

    add network tool
    Signed-off-by: 蒙牛D罩杯撸废 <rangeChiong@outlook.com>

---
+++ RWKit/Helper/RWRequest.h
@@ -0,0 +1,71 @@
+//
+//  RWRequest.h
+//  Test0625
+//
+//  Created by 常小哲 on 16/6/25.
+//  Copyright © 2016年 常小哲. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFNetworking.h"
+
+typedef NS_ENUM(NSInteger, RequestNormalType) {
+    RequestNormalType_Get = 0,
+    RequestNormalType_Post,
+    RequestNormalType_Put,
+    RequestNormalType_Delete
+};
+
+@interface RWRequest : NSObject
+
++ (instancetype)shareRequest;
+
+/*!
+ *  设置请求超时时间
+ */
+- (void)setTimeoutInterval:(NSTimeInterval)timeoutInterval;
+
+/*!
+ *  根据type请求
+ */
+- (void)request:(NSString *)url
+           type:(RequestNormalType)type
+         params:(NSDictionary *)params
+        success:(void (^)(NSDictionary *dict))success
+        failure:(void (^)(NSError *error))failure;
+
+/*!
+ *  上传文件
+ */
+- (void)post:(NSString *)url
+      params:(NSDictionary *)params
+        body:(void (^)(id<AFMultipartFormData> formData))body
+     success:(void (^)(NSDictionary* dic))success
+     failure:(void (^)(NSError *error))failure;
+
+/*!
+ *  取消网络请求
+ */
+- (void)cancelAllRequest;
+
+/*!
+ *  监听网络状态
+ */
++ (void)observeNetworkStatus:(void (^)(AFNetworkReachabilityStatus status))block;
+
+/*!
+ *  移除网络状态监听
+ */
++ (void)removeNetworkStatusObserver;
+
+/*!
+ *  开启请求的URLcache
+ */
++ (void)openRequsetCache;
+
+/*!
+ *  移除所有的URLcache
+ */
++ (void)removeAllCachedResponses;
+
+@end
---
+++ RWKit/Helper/RWRequest.m
@@ -0,0 +1,185 @@
+//
+//  RWRequest.m
+//  Test0625
+//
+//  Created by 常小哲 on 16/6/25.
+//  Copyright © 2016年 常小哲. All rights reserved.
+//
+
+#import "RWRequest.h"
+
+@implementation NSData (utils)
+
+- (NSDictionary *)parseJson {
+    NSError *error;
+    NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:self
+                                                         options:NSJSONReadingMutableContainers|NSJSONReadingMutableLeaves
+                                                           error:&error];
+    return dict;
+}
+
+@end
+
+static NSString *const TimeOutKeyPath = @"timeoutInterval";
+
+@interface RWRequest () {
+    AFHTTPSessionManager *_requestManager;
+}
+
+@end
+
+@implementation RWRequest
+
++ (instancetype)shareRequest {
+    static dispatch_once_t once;
+    static id instance;
+    dispatch_once(&once, ^{
+        instance = [self new];
+    });
+    return instance;
+}
+
+- (instancetype)init {
+    if (self = [super init]) {
+        _requestManager = [AFHTTPSessionManager manager];
+        _requestManager.requestSerializer = [AFHTTPRequestSerializer serializer];
+        _requestManager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    }
+    return self;
+}
+
+- (void)setTimeoutInterval:(NSTimeInterval)timeoutInterval {
+    [_requestManager.requestSerializer willChangeValueForKey:TimeOutKeyPath];
+    _requestManager.requestSerializer.timeoutInterval = timeoutInterval;
+    [_requestManager.requestSerializer didChangeValueForKey:TimeOutKeyPath];
+
+}
+
+#pragma mark-   request
+
+- (void)request:(NSString *)url
+           type:(RequestNormalType)type
+         params:(NSDictionary *)params
+        success:(void (^)(NSDictionary *dict))success
+        failure:(void (^)(NSError *error))failure {
+    
+    // 请求成功的回调
+    void (^successfulRequest) (NSURLSessionDataTask *, id) = ^(NSURLSessionDataTask *task, id responseObject) {
+        if (success) {
+            NSDictionary *dict = [responseObject parseJson];
+            success(dict);
+        }
+    };
+    
+    // 请求失败的回调
+    void (^failedRequest) (NSURLSessionDataTask *, NSError *) = ^(NSURLSessionDataTask *operation, NSError *error) {
+        if (failure) failure(error);
+    };
+    
+    switch (type) {
+        case RequestNormalType_Get: {
+            [_requestManager GET:url
+                      parameters:params
+                        progress:nil
+                         success:successfulRequest
+                         failure:failedRequest];
+        }
+            break;
+            
+        case RequestNormalType_Post: {
+            [_requestManager POST:url
+                       parameters:params
+                         progress:nil
+                          success:successfulRequest
+                          failure:failedRequest];
+        }
+            break;
+            
+        case RequestNormalType_Put: {
+            [_requestManager PUT:url
+                      parameters:params
+                         success:successfulRequest
+                         failure:failedRequest];
+        }
+            break;
+            
+        case RequestNormalType_Delete: {
+            [_requestManager DELETE:url
+                         parameters:params
+                            success:successfulRequest
+                            failure:failedRequest];
+        }
+            break;
+            
+        default:
+            break;
+    }
+}
+
+- (void)post:(NSString *)url
+      params:(NSDictionary *)params
+        body:(void (^)(id<AFMultipartFormData> formData))body
+     success:(void (^)(NSDictionary* dic))success
+     failure:(void (^)(NSError *error))failure {
+    
+    [_requestManager POST:url parameters:params constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        if (body) body(formData);
+    }
+         progress:nil
+          success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+              if (success) {
+                  NSDictionary *dict = [responseObject parseJson];
+                  success(dict);
+              }
+    }
+          failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+              if (failure) failure(error);
+    }];
+}
+
+- (void)cancelAllRequest {
+    [_requestManager.operationQueue cancelAllOperations];
+}
+
+#pragma mark-  download
+
+
+
+
+#pragma mark-  network status
+
++ (void)observeNetworkStatus:(void (^)(AFNetworkReachabilityStatus status))block {
+    [[AFNetworkReachabilityManager sharedManager] startMonitoring];
+    
+    [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+            if (block)
+                block(status);
+    }];
+}
+
++ (void)removeNetworkStatusObserver {
+    [[AFNetworkReachabilityManager sharedManager] stopMonitoring];
+}
+
+#pragma mark-  cache
+
++ (void)openRequsetCache {
+    NSURLCache *URLCache = [[NSURLCache alloc] initWithMemoryCapacity:4 * 1024 * 1024
+                                                         diskCapacity:20 * 1024 * 1024
+                                                             diskPath:nil];
+    [NSURLCache setSharedURLCache:URLCache];}
+
++ (void)removeAllCachedResponses {
+    [[NSURLCache sharedURLCache] removeAllCachedResponses];
+
+}
+
+@end
+
+
+
+
+
+
+
+
