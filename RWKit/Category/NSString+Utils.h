83f58498b1f55c3904b5c82aa8179a49f48b781f
常润泽 <changrz1@centaline.com.cn>
2016-06-28 16:22:30 +0800

    add check phone number tool and screen snapshot tool
    Signed-off-by: 常润泽 <changrz1@centaline.com.cn>

---
+++ RWKit/Category/NSString+Utils.h
@@ -0,0 +1,16 @@
+//
+//  NSString+Utils.h
+//  Pods
+//
+//  Created by Ranger on 16/6/28.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Utils)
+
+/** 正则匹配11位手机号码 */
+- (BOOL)rw_checkPhoneNumber;
+
+@end
---
+++ RWKit/Category/NSString+Utils.m
@@ -0,0 +1,40 @@
+//
+//  NSString+Utils.m
+//  Pods
+//
+//  Created by Ranger on 16/6/28.
+//
+//
+
+#import "NSString+Utils.h"
+
+@implementation NSString (Utils)
+
+#pragma 正则匹配11位手机号
+- (BOOL)rw_checkPhoneNumber {
+    
+    NSString * MOBILE = @"^1(3[0-9]|5[0-35-9]|8[025-9])\\d{8}$";
+    
+    NSString * CM = @"^1(34[0-8]|(3[5-9]|5[017-9]|8[2378])\\d)\\d{7}$";
+    
+    NSString * CU = @"^1(3[0-2]|5[256]|8[56])\\d{8}$";
+    
+    NSString * CT = @"^1((33|53|8[09])[0-9]|349)\\d{7}$";
+    
+    NSPredicate *regextestmobile = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", MOBILE];
+    NSPredicate *regextestcm = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CM];
+    NSPredicate *regextestcu = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CU];
+    NSPredicate *regextestct = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", CT];
+    BOOL res1 = [regextestmobile evaluateWithObject:self];
+    BOOL res2 = [regextestcm evaluateWithObject:self];
+    BOOL res3 = [regextestcu evaluateWithObject:self];
+    BOOL res4 = [regextestct evaluateWithObject:self];
+    
+    if (res1 || res2 || res3 || res4 ) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
+
+@end
